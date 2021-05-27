91dbc893c1e948d5cc7084c5fd8c8af8bce69b61
Oguz Karahanoglu <karahanogluoguz@gmail.com>
2021-05-27 15:08:45 +0200

    search function

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,11 @@ class Boat < ApplicationRecord
   has_many :bookings
 
   belongs_to :user
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location,
+    against: [:location],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
